4a495927411763458ae3e29b5667cee43c95bc99
Ruslan2363 <99345596+Ruslan2363@users.noreply.github.com>
2023-01-30 13:30:24 +0200

    Update NokiaPhone.java

--- src/NokiaPhone.java
+++ src/NokiaPhone.java
@@ -1,15 +1,51 @@
 public class NokiaPhone extends Phone implements PhoneConnection{
-    public NokiaPhone(String name, String model, String color, int ram, int storage) {
-        super(name, model, color, ram, storage);
+    private String numNokia;
+    private String processor;
+
+    public String getNumNokia() {
+        return numNokia;
     }
+
+    public void setNumNokia(String numNokia) {
+        this.numNokia = numNokia;
+    }
+
+    public String getProcessor(){
+        return processor ;
+    }
+
+    public void setProcesor(String procesor) {
+        this.processor = procesor;
+    }
+
+    public NokiaPhone(String name, String model, String color, int ram, int storage,String numNokia,String processor) {
+        super(name, model, color, ram, storage);
+        this.processor=processor;
+        this.numNokia=numNokia;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString()+"NokiaPhone{" +
+                "numNokia='" + numNokia + '\'' +
+                ", processor='" + processor + '\'' +
+                '}';
+    }
+
+
 
     @Override
     public void call() {
+
+        System.out.println( numNokia+"Телефонуємо");
 
     }
 
     @Override
     public void sendMassage() {
+        System.out.println( numNokia+"Надсилаємо");
 
     }
 }
